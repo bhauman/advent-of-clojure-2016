dd24ab1e9c367248974f419a1497e4836bd2589d
Bruce Hauman <bhauman@gmail.com>
2016-12-15 16:11:07 -0500

    made better names

--- src/advent_of_clojure_2016/day15.clj
+++ src/advent_of_clojure_2016/day15.clj
@@ -7,22 +7,21 @@
                   [1 5]
                   [0 3]])
 
-(defn item-at-t [[pos md] t] [(mod (+ t pos) md) md])
+(defn disc-at-t [[pos disc-size] t]
+  [(mod (+ t pos) disc-size) disc-size])
 
-(defn tick [s t]
-  (map-indexed (fn [i item] (item-at-t item (+ 1 t i))) s))
+(defn state-of-discs-at-t [s t]
+  (map-indexed (fn [i disc] (disc-at-t disc (+ 1 t i))) s))
 
 (def completed #(every? zero? (map first %)))
 
 (defn times-with-aligned-discs [s start step]
-  (eduction
-   (filter #(completed (tick s %)))
-   (range start Integer/MAX_VALUE step)))
+  (->> (range start Integer/MAX_VALUE step)
+       (filter #(completed (state-of-discs-at-t s %)))))
 
 (defn next-step [{:keys [pos step]} state]
   (let [[next-pos after-pos] (take 2 (times-with-aligned-discs state pos step))]
-    {:pos next-pos
-     :step (- after-pos next-pos)}))
+    {:pos next-pos :step (- after-pos next-pos)}))
 
 (defn find-answer [state]
   (reduce next-step {:pos 0 :step 1} (rest (reductions conj [] state))))
