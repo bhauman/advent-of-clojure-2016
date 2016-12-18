aa7659b45a102fd59ca50d9f87e18a2591eb930a
Bruce Hauman <bhauman@gmail.com>
2016-12-18 00:35:40 -0500

    clean day 16 a bit

--- src/advent_of_clojure_2016/day16.clj
+++ src/advent_of_clojure_2016/day16.clj
@@ -1,11 +1,12 @@
 (ns advent-of-clojure-2016.day16)
 
+(def input "11110010111001001")
+
 (defn expand [a]
   (concat a (cons \0 (map {\1 \0 \0 \1} (reverse a)))))
 
 (defn expand-until [size initial]
-  (take size
-        (first (filter #(>= (count %) size) (iterate expand initial)))))
+  (first (filter #(>= (count %) size) (iterate expand initial))))
 
 (defn check-sum* [s]
   (map (fn [[a b]] (if (= a b) \1 \0)) (partition 2 s)))
@@ -13,13 +14,17 @@
 (defn check-sum [s]
   (first (filter (comp odd? count) (iterate check-sum* s))))
 
-(def find-answer (comp (partial apply str) check-sum expand-until))
+(defn find-answer [size s]
+  (->> (expand-until size s)
+       (take size)
+       check-sum
+       (apply str)))
 
-#_(find-answer 272 "11110010111001001")
+#_(find-answer 272 input)
 ;; => 01110011101111011
 
-; not performant (222 seconds) but 35Megs isn't impossible
-; structural sharing and seqs perform their magic here
-#_ (def res  (time (find-answer 35651584 "11110010111001001")))
+;; not performant (222 seconds) but 35Megs isn't impossible
+;; structural sharing and seqs perform their magic here
+#_ (def res (time (find-answer 35651584 input)))
 ;; => 11001111011000111
 
