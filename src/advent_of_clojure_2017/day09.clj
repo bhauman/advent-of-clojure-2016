12b6436c38689d66b4a5ba5abc206c69f0527450
Bruce Hauman <bhauman@gmail.com>
2017-12-09 10:15:36 -0500

    day 9 much simpler and shorter

--- src/advent_of_clojure_2017/day09.clj
+++ src/advent_of_clojure_2017/day09.clj
@@ -5,54 +5,29 @@
 
 (def data (slurp (io/resource "2017/day09")))
 
-(defn remove-trash [data]
-  (reduce
-   (fn [{:keys [out trash] :as state} ch]
-     (cond
-       (and (empty? trash) (not= ch \<))
-       (update state :out conj ch)
-       (empty? trash) ;; and \< is implied here
-       (update state :trash conj ch)
-       (odd? (count (take-while #{\!} trash)))
-       (update state :trash conj ch)              
-       (= ch \>) (assoc state :trash '())
-       (= ch \!) (update state :trash conj ch) ;; ADDED FOR PART 2
-       :else
-       (-> state
-           (update :trash-count inc) ;; ADDED FOR PART 2
-           (update :trash conj ch))))
-   {:trash '()
-    :trash-count 0 
-    :out []}
-   data))
-
-(defn prep-data [data]
-  (-> (apply str (:out (remove-trash data))) 
-      (string/replace "," "")
+(defn parse-structure [data]
+  (-> data
+      ;; eliminate escapes
+      (string/replace #"(!+)(.)" #(if (odd? (count (second %))) "" (last %)))
+      ;; replace garbage with garbage count
+      (string/replace #"<[^>]*>" (comp str dec dec count))
       (string/replace "{" "(")
       (string/replace "}" ")")
+      ;; parse as clojure data!!
       read-string))
 
-(defn count-em
-  ([data] (count-em data 1))
+(defn score
+  ([data] (score data 1))
   ([data depth]
-   (if (empty? data)
-     depth
-     (+ depth (apply + (map #(count-em % (inc depth)) data))))))
-
-(def part-1 (comp count-em prep-data))
+   (cond
+     (number? data) 0
+     (empty? data) depth
+     :else (+ depth (apply + (map #(count-em % (inc depth)) data))))))
 
 ;; part 1
-#_(part-1 data)
+#_ (time (score (parse-structure data)))
 ;;=> 13154
 
 ;; part 2
-#_ (:trash-count (remove-trash data))
+#_ (apply + (flatten (parse-structure data)))
 ;;=> 6369
-
-
-
-
-
-
-
