68a4ba981b4561577be918b286c385a557e1ccf7
Bruce Hauman <bhauman@gmail.com>
2017-12-12 06:57:14 -0500

    day 12

---
+++ src/advent_of_clojure_2017/day12.clj
@@ -0,0 +1,45 @@
+(ns advent-of-clojure-2017.day11
+  (:require
+   [clojure.java.io :as io]
+   
+   [clojure.set :as st]))
+
+(def data (->> (io/resource "2017/day12")
+               io/reader
+               line-seq
+               (map #(format "[%s]" %))
+               (map read-string)))
+
+(defn index [data']
+  (reduce (fn [accum [node _ & direct-connect]]
+            (as-> (set direct-connect) x
+              (disj x node)
+              (assoc accum node x)))
+          {}
+          data'))
+
+;; find group without cycles
+
+(defn group [idx root]
+  (into #{}
+        (tree-seq (let [seen (atom #{})]
+                    (fn [x] (when-not (@seen x)
+                              (swap! seen conj x))))
+                  idx
+                  root)))
+
+;; part 1
+#_(let [idx (index data)]
+    (time (count (group idx 0))))
+;; => 113
+
+;; part 2
+;; not worth optimizing for problem space
+#_(let [idx (index data)]
+    (->> (map #(group idx %) (keys idx))
+         (into #{})
+         count))
+;; => 202
+
+
+
