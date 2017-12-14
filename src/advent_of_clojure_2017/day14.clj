92866b3c3a7994eb60059c4ca363916a8f766aaa
Bruce Hauman <bhauman@gmail.com>
2017-12-14 10:31:16 -0500

    day 14

---
+++ src/advent_of_clojure_2017/day14.clj
@@ -0,0 +1,72 @@
+(ns advent-of-clojure-2017.day14
+  (:require
+   [advent-of-clojure-2017.day10 :refer [sparse-hash dense-hash]]))
+
+(def test-input "flqrgnkx")
+
+(def puzzle-input "oundnydw")
+
+(defn binary-dense-hash [sparseh]
+  (->> sparseh
+       (partition 16)
+       (map #(reduce bit-xor %))
+       (map #(Integer/toBinaryString %))
+       (map #(format "%8s" %))
+       (apply str)))
+
+(def knot-hash (comp binary-dense-hash sparse-hash (partial map int)))
+
+(defn disk-sector-map [input]
+  (->> (map #(str %1 "-" %2) (repeat input) (range 128))
+       (mapv knot-hash)))
+
+;; part 1
+#_(->> (disk-sector-map puzzle-input)
+       (map (comp count (partial filter #{\1})))
+       (reduce +)
+       time)
+;; => 8106
+
+(def data (->> (disk-sector-map puzzle-input)
+               (mapv (partial mapv {\1 1 \space 0 \0 0}))))
+
+(def directions [[0 1] [0 -1] [1 0] [-1 0]])
+
+(defn pos->directions [pos]
+  (->> (mapv #(mapv + pos %)  directions)
+       ;; not really needed but saves a few steps
+       (filter (fn [[y x]] (and
+                            (< -1 y 128)
+                            (< -1 y 128))))))
+
+(defn children [data pos]
+  (doall
+   (filter #(= 1 (get-in data %))
+           (pos->directions pos))))
+
+(defn group [data chidren-fn pos]
+  (set (tree-seq
+         (let [seen (atom #{})]
+           (fn [x] (when-not (@seen x)
+                     (do
+                       (swap! seen conj x)
+                       (and
+                        (not= 0 (get-in data x))
+                        (not-empty (chidren-fn x)))))))
+         chidren-fn
+         pos)))
+
+(defn all-groups [data]
+  (let [children-fn (memoize (partial children data))]
+    (into #{}
+          (for [x (range 128)
+                y (range 128)
+                :let [pos [y x]]
+                :when (not (zero? (get-in data pos)))]
+            (group data children-fn pos)))))
+
+;; part 2
+#_(time (count (all-groups data)))
+;; Elapsed time: 3957.949806 msecs
+;; => 1164
+
