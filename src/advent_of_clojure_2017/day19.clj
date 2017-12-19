aa35b1bc515dd2616cf405730ac746ace2bb1baa
Bruce Hauman <bhauman@gmail.com>
2017-12-19 15:18:39 -0500

    refactor

--- src/advent_of_clojure_2017/day19.clj
+++ src/advent_of_clojure_2017/day19.clj
@@ -13,47 +13,45 @@
 (defn letter? [char']
   (<= (int \A) (int char') (int \Z)))
 
-(defn char-at-position [char-map [y x]]
+(defn char-at-position [char-map [y x :as pos]]
   (.charAt ^String (get char-map y) x))
 
 (defn starting-x [char-map]
   (.indexOf (first char-map) (int \|)))
 
-(defn next-directions [char-map [pos current-direction]]
+(defn next-directions [char-map [pos prev-pos]]
   (if (not= \+ (char-at-position char-map pos))
-    [current-direction]
-    (let [opposite-direction (map - current-direction)]
-      (filter #(not= % opposite-direction) directions))))
+    [(mapv - pos prev-pos)]
+    directions))
 
-(defn next-positions [char-map [pos :as state]]
+(defn next-position [char-map [pos prev-pos :as state]]
   (->> (next-directions char-map state)
        (mapv (partial mapv + pos))
-       (filter #(not= \space (char-at-position char-map %)))))
+       (filter #(not= prev-pos %))
+       (filter #(not= \space (char-at-position char-map %)))
+       first))
 
 (defn move [char-map [pos :as state]]
-  (assert (not= \space (char-at-position char-map pos)))
-  (let [candidate-positions (next-positions char-map state)]
-    (assert (#{0 1} (count candidate-positions)))
-    (when-let [new-pos (first candidate-positions)]
-        [new-pos (mapv - new-pos pos)])))
+  [(next-position char-map state) pos])
 
-#_(->> (iterate (partial move data) [[0 (starting-x data)] [1 0]])
-       (take-while identity)
+#_(->> (iterate (partial move data)
+                [[0 (starting-x data)] [-1 (starting-x data)]])
        (map first)
+       (take-while identity)
        (map (partial char-at-position data))
        (filter letter?)
        (apply str)
        time)
 ;; part 1
 ;; => EOCZQMURF
-;; Elapsed time: 117.173515 msecs
+;; Elapsed time: 80.573515 msecs
 
 ;; part 2
 #_(->> (iterate (partial move data)
-                [[0 (starting-x data)]
-                 [1 0]])
+                [[0 (starting-x data)] [-1 (starting-x data)]])
+       (map first)
        (take-while identity)
        count
        time)
 ;; => 16312
-;; Elapsed time: 98.812626 msecs
+;; Elapsed time: 72.912626 msecs
